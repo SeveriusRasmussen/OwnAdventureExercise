451f741952e0ac2504dc083d92385f30a58cbaf6
SeveriusRasmussen <sora0001@stud.kea.dk>
2023-10-10 15:09:27 +0200

    Inventory metode and examine

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -1,3 +1,5 @@
+import AdditionStuffs.Colors;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -27,7 +29,9 @@ public class UserInterface {
         System.out.println("""
                 \033[0;33mWelcome to the house of the void
                 You are here to find the dimension cube to get away from the void.
-                You are standing in front of the front door of the house.
+                You are standing in front of the frontdoor of the house.
+                A voice from the void is telling you \033[0;34m''...ype... help.... to... figu... thin... ut...''\033[0;33m
+                You can't quite figure out what he want but the word ''help'' sounds helpful.
                 Then you enter the house and are now standing in:\033[0m
                 """ + getCurrentRoomName());
 
@@ -42,39 +46,39 @@ public class UserInterface {
         switch (input.toLowerCase()) {
 
             // Direction metode
-            case "N", "n", "north":
+            case "n", "north":
                 Room newRoomNorth = gameMap.getCurrent().getNeighbourNorth();
                 movePlayer(newRoomNorth);
                 //movePlayer("north");
                 break;
 
-            case "S", "s", "south":
+            case "s", "south":
                 Room newRoomSouth = gameMap.getCurrent().getNeighbourSouth();
                 movePlayer(newRoomSouth);
                 //movePlayer("south");
                 break;
 
-            case "E", "e", "east":
+            case "e", "east":
                 Room newRoomEast = gameMap.getCurrent().getNeighbourEast();
                 movePlayer(newRoomEast);
                 //movePlayer("east");
                 break;
 
-            case "W", "w", "west":
+            case "w", "west":
                 Room newRoomWest = gameMap.getCurrent().getNeighbourWest();
                 movePlayer(newRoomWest);
                 //movePlayer("west");
                 break;
 
             // Take item metode
-            case "TAKE", "take":
+            case "t", "take":
                 System.out.println("Type the name of the item you wish to take:");
                 String itemToTake = scanner.nextLine();
                 takeItem(itemToTake);
                 break;
 
             // Drop item metode
-            case "DROP", "drop":
+            case "d", "drop":
                 System.out.println("Type the name of the item you wish to drop from your inventory.");
                 String itemToDrop = scanner.nextLine();
                 dropItem(itemToDrop);
@@ -86,24 +90,24 @@ public class UserInterface {
                 break;
 
             // Investigate item metode
-            case "EXAMINE", "examine":
+            case "examine":
                 System.out.println("Enter the name of the item you want to examine:");
                 String itemToExamine = scanner.nextLine();
                 examineItem(itemToExamine);
                 break;
 
             // Quit metode
-            case "Q", "q", "quit":
+            case "q", "quit":
                 quitGame();
                 break;
 
             // help metode
-            case "H", "h", "help":
+            case "h", "help":
                 displayHelp();
                 break;
 
             // Look metode
-            case "L", "l", "look":
+            case "l", "look":
                 Adventure.lookAroundRoom(gameMap.getCurrent()); //Call the lookAroundRoom method
                 break;
 
@@ -130,12 +134,15 @@ public class UserInterface {
 
     private void displayHelp() {
         System.out.println("""
-                Write one of these commands to take action:
-                Movements:
-                N or North to move north, S or South to move south, W or West to move west, E or East to move east.
-                not available: T or Take to take item from the room you're in
-                not available: D or Drop to drop item from your inventory into the room you're in.
-                L or Look to observe the room you're in.
+                The void is echoing in your head telling you to type these to use in the house:
+                For the movements:
+                N or north: move north, S or south: move south, W or west: move west, E or east: move east.
+                For the other commands:
+                T or take: to take item from the room you're in.
+                D or drop: to drop the item from your inventory into the room you're in.
+                Inv or bag: look into the inventory of your bag.
+                Examine: take a closer look at the item you chosen from the bag.
+                L or Look: to observe the room you're in.
                 Q or Quit to exit the game.""");
     }
 
@@ -187,7 +194,7 @@ public class UserInterface {
     }
 
     public void examineItem(String itemName) {
-        // Check if the plyaer has the specified item
+        // Check if the player has the specified item
         ArrayList<Item> inventory = player.getInventory();
         for (Item item : inventory) {
             if (item.getName().equalsIgnoreCase(itemName)) {
@@ -199,76 +206,4 @@ public class UserInterface {
         System.out.println("Item not found in your inventory.");
     }
 }
-/*
 
-import java.util.Scanner;
-
-public class UserInterface {
-    public void start() {
-        Map gameMap = new Map();
-        gameMap.buildMap();
-
-        Scanner scanner = new Scanner(System.in);
-
-        // Intro.
-        System.out.println("""
-                \033[0;33mWelcome to the house of the void
-                You are here to find the dimension cube to get away from the void.
-                You are standing in front of the front door of the house.
-                Then you enter the house and are now standing in:\033[0m
-                """ + gameMap.getCurrent());
-
-        // Player input
-        while (true) {
-            String input = scanner.next().toUpperCase();
-
-            // Metode for moving the player
-            Room currentRoom = gameMap.getCurrent();
-            Room newRoom = null;
-
-            switch (input.toLowerCase()) {
-                case "N", "n", "north":
-                    newRoom = currentRoom.getNeighbourNorth();
-                    break;
-                case "S", "s", "south":
-                    newRoom = currentRoom.getNeighbourSouth();
-                    break;
-                case "W", "w", "west":
-                    newRoom = currentRoom.getNeighbourWest();
-                    break;
-                case "E", "e", "east":
-                    newRoom = currentRoom.getNeighbourEast();
-                    break;
-                // Quit metode
-                case "Q", "q", "quit":
-                    System.out.println("\033[0;31mYou voided your life into the infinite dimension.\033[0m");
-                    System.exit(0); // afslut programmet.
-                    break;
-                // Help metode
-                case "H", "h", "help":
-                    System.out.println("""
-                    Write one of these typo to get the action:
-                    N or n or north to walk forward north, S or s or south to go for south,
-                    W or w or west to go west, E or e or east to go east.
-                    L for look around in the room you're in.
-                    Q for quit the game, only god knows what will happens.""");
-                    continue;
-                // Look metode
-                case "L", "l", "look":
-                    System.out.println(gameMap.getCurrentDescription());
-                    continue;
-                default:
-                    System.out.println("You... you... ASSHOLE. Input something useful now!");
-                    continue;
-           }
-
-           // metode for the player moving into a room or against the wall.
-           if (newRoom != null) {
-                gameMap.setCurrent(newRoom);
-                System.out.println("You are now in " + gameMap.getCurrentRoom());
-           } else {
-               System.out.println("You ran into the wall, how stupid are you really?");
-           }
-        }
-    }
-} */
